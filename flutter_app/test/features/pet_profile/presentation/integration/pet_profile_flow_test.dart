f8f459d7920892a0d3f3471ff0e5015a2cef9832
Fred <frederique.prevost@pm.me>
2026-03-26 22:47:15 +0000

    fix

--- flutter_app/test/features/pet_profile/presentation/integration/pet_profile_flow_test.dart
+++ flutter_app/test/features/pet_profile/presentation/integration/pet_profile_flow_test.dart
@@ -167,27 +167,5 @@ void main() {
 
       expect(find.text('Buddy'), findsOneWidget, reason: 'Should show Buddy in the list');
     });
-
-// --- TEST NOTIFIER FOR ADD PET ---
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-class PetListTestNotifier extends StateNotifier<AsyncValue<List<dynamic>>> {
-  PetListTestNotifier() : super(const AsyncValue.data([]));
-  final provider = StateNotifierProvider<PetListTestNotifier, AsyncValue<List<dynamic>>>((ref) => PetListTestNotifier());
-  ProviderOverride get override => provider.overrideWithValue(this);
-  void addPet(String name) {
-    state = AsyncValue.data([_TestPet(name)]);
-  }
-}
-class _TestPet {
-  final String name;
-  _TestPet(this.name);
-  String get id => name.toLowerCase();
-  String get species => 'Dog';
-  String get breed => '';
-  String get bio => '';
-  int get colorValue => 0xFF7E57C2;
-  bool get passedAway => false;
-  String? get photoPath => null;
-}
   });
 }
