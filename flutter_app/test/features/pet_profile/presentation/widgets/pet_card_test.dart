6a6928752cc4e2fdcc0db904bb88212394b69499
Fred <frederique.prevost@pm.me>
2026-03-26 21:56:27 +0000

    fix teh pet card test

--- flutter_app/test/features/pet_profile/presentation/widgets/pet_card_test.dart
+++ flutter_app/test/features/pet_profile/presentation/widgets/pet_card_test.dart
@@ -4,19 +4,63 @@ import 'package:pet_profile_app/features/pet_profile/domain/entities/pet.dart';
 import 'package:pet_profile_app/features/pet_profile/presentation/widgets/pet_card.dart';
 
 void main() {
+
   const testPet = Pet(
     id: 'test-id',
     name: 'Buddy',
     species: 'Dog',
     breed: 'Golden Retriever',
     bio: 'A friendly dog',
+    photoPath: '',
+    colorValue: 0xFF7E57C2,
+    passedAway: false,
   );
 
   const petNoBio = Pet(
     id: 'test-id-2',
     name: 'Whiskers',
     species: 'Cat',
+    breed: '',
+    bio: '',
+    photoPath: '',
+    colorValue: 0xFF26A69A,
+    passedAway: false,
   );
+    test('throws if required fields are null', () {
+      expect(
+        () => Pet(
+          id: 'id',
+          name: '',
+          species: '',
+        ),
+        returnsNormally,
+      );
+      // The following should not compile, but we can check runtime null assertion
+      expect(
+        () => Pet(
+          id: 'id',
+          name: null as String, // ignore: null_argument_to_non_null_type
+          species: 'Dog',
+        ),
+        throwsA(isA<AssertionError>()),
+      );
+      expect(
+        () => Pet(
+          id: null as String, // ignore: null_argument_to_non_null_type
+          name: 'Buddy',
+          species: 'Dog',
+        ),
+        throwsA(isA<AssertionError>()),
+      );
+      expect(
+        () => Pet(
+          id: 'id',
+          name: 'Buddy',
+          species: null as String, // ignore: null_argument_to_non_null_type
+        ),
+        throwsA(isA<AssertionError>()),
+      );
+    });
 
   Widget createTestWidget(Widget child) {
     return MaterialApp(
